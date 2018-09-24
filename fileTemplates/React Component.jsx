d2351c762c2ad1e3c2b33d4c20424244954ea45c
Dzianis Roi <mcreader215@gmail.com>
2018-09-24 22:29:10 +0300

    WS-183.2635.17 <mcreader@Dzianiss-MacBook-Pro.local Overwrite remote https://github.com/McReader/webstorm-settings.git to local

--- fileTemplates/React Component.jsx
+++
@@ -1,9 +0,0 @@
-import {Component} from 'react';
-
-export default class #[[$Name$]]# extends Component {
-
-  render() {
-      return null;
-  }
-    
-}
--- fileTemplates/internal/JavaScript File.js
+++
@@ -1,5 +0,0 @@
-/**
- * DATAWATCH NEW SERVER
- *
- * #[[$Description$]]#
- */
